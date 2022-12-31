c654c896795035ca3c9460dde9835ffe50851a42
Tapabrata Barick <93761910+TheTapo@users.noreply.github.com>
2022-12-31 10:21:17 +0000

    Add files via upload

--- nvim/lua/keybinds/init.lua
+++ nvim/lua/keybinds/init.lua
@@ -1,4 +1,9 @@
 vim.g.mapleader = ' ' 
 local map = vim.api.nvim_set_keymap
-map('n', '<leader>e', ':CHADopen<CR>', {noremap = true, silent = true})
+map('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 map('n', '<leader>f',':Telescope oldfiles<CR>',{noremap = true, silent =true})
+map('n', '<leader>t', ':ToggleTerm<CR>', {noremap = true, silent = true})
+map('n', '<leader>h', '<C-w>h', {noremap = true, silent = true})
+map('n', '<leader>j', '<C-w>j', {noremap = true, silent = true})
+map('n', '<leader>k', '<C-w>k', {noremap = true, silent = true})
+map('n', '<leader>l', '<C-w>l', {noremap = true, silent = true})
